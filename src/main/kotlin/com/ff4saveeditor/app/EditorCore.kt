22413bb4754b50407289cb6706d9734fe01de650
Keyon Amirpanahi <keyon@keyoninfo.com>
2020-05-19 13:37:58 -0500

    Fix window size

--- src/main/kotlin/com/ff4saveeditor/app/EditorCore.kt
+++ src/main/kotlin/com/ff4saveeditor/app/EditorCore.kt
@@ -1,6 +1,14 @@
 package com.ff4saveeditor.app
 
 import com.ff4saveeditor.view.MainView
+import javafx.stage.Stage
 import tornadofx.App
 
-class EditorCore: App(MainView::class, Styles::class)
+class EditorCore: App(MainView::class, Styles::class) {
+    override fun start(stage: Stage) {
+        super.start(stage)
+        stage.width = 800.0
+        stage.height = 600.0
+        stage.isResizable = false
+    }
+}
